23ab00a3136f67de0b7a1b57cb0c63dd9c3ad2ba
Victor R Santos <vrsisa@gmail.com>
2025-06-03 08:46:21 -0300

    first commit

---
+++ ContaOnline.Domain/Interfaces/IContaCategoriaRepository.cs
@@ -0,0 +1,14 @@
+﻿using ContaOnline.Domain.Models;
+using System.Collections.Generic;
+
+namespace ContaOnline.Domain.Interfaces
+{
+    public interface IContaCategoriaRepository
+    {
+        void Incluir(ContaCategoria contaCategoria);
+        void Alterar(ContaCategoria contaCategoria);
+        void Excluir(string id);
+        ContaCategoria ObterPorId(string id);
+        IEnumerable<ContaCategoria> ObterTodos();
+    }
+}
---
+++ ContaOnline.Domain/Models/ContaCategoria.cs
@@ -0,0 +1,8 @@
+﻿namespace ContaOnline.Domain.Models
+{
+    public class ContaCategoria
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
