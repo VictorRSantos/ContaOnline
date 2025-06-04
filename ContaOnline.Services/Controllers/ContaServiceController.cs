f6787b252369f1a3279a509ce84d729548029658
Victor R Santos <vrsisa@gmail.com>
2025-06-04 08:44:03 -0300

    Criando Servico

---
+++ ContaOnline.Services/Controllers/ContaServiceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContaOnline.Services.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContaServiceController : ControllerBase
+    {
+        // GET: api/<ContaServiceController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }        
+    }
+}
