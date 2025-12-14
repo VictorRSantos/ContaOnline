05a3601f9ae58d9c6836c9efa9bc3ea76464f6ef
Victor R Santos <vrsisa@hotmail.com>
2025-12-14 14:27:05 -0300

    Implementando registro de usuário

--- ContaOnline.UI.Web/Controllers/AppController.cs
+++ ContaOnline.UI.Web/Controllers/AppController.cs
@@ -3,12 +3,13 @@ using ContaOnline.Domain.Models;
 using ContaOnline.Repository;
 using ContaOnline.UI.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
 
 namespace ContaOnline.UI.Web.Controllers
 {
     public class AppController : Controller
     {
-       
+
         public ActionResult Login()
         {
             var loginViewModel = new LoginViewModel();
@@ -48,6 +49,55 @@ namespace ContaOnline.UI.Web.Controllers
         public IActionResult Registro()
         {
             var registro = new RegistroViewModel();
+            return View(registro);
+        }
+
+        [HttpPost]
+        public IActionResult Registro(RegistroViewModel registro)
+        {
+            if (string.IsNullOrEmpty(registro.Email))
+            {
+                ModelState.AddModelError("Email", "O email deve ser informado");
+            }
+
+            if (string.IsNullOrEmpty(registro.Senha))
+            {
+                ModelState.AddModelError("Sennha", "A senha deve ser informado");
+            }
+            else
+            {
+                if (registro.Senha != registro.ConfirmarSenha)
+                {
+                    ModelState.AddModelError("ConfirmarSenha", "As senhas não coincidem.");
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                //IUsuarioRepository repositorio = AppHelper.ObterUsuarioRepository();
+                //Usuario usuarioExistente = repositorio.ObterPorEmailSenha(registro.Email);
+                //if (usuarioExistente != null)
+                //{
+                //    ModelState.AddModelError("Email", "Já existe um usuário cadastrado com este email.");
+                //    return View(registro);
+                //}
+
+                var usuarioRepositorio = AppHelper.ObterUsuarioRepository();
+                var novoUsuario = new Usuario
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Nome = registro.Nome,
+                    Email = registro.Email,
+                    Senha = registro.Senha
+                };
+
+                usuarioRepositorio.Incluir(novoUsuario);
+                AppHelper.RegistrarUsuario(novoUsuario);
+
+                return RedirectToAction("Inicio");
+            }
+
+
             return View(registro);
         }
 
