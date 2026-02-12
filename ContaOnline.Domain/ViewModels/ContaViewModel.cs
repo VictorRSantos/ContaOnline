44919f5c5f6612ceabcf4dd38e194723a5e47fdd
Victor R Santos <vrsisa@hotmail.com>
2026-02-12 00:23:34 -0300

    Implementacao Incluir e Alterar dados da conta

---
+++ ContaOnline.Domain/ViewModels/ContaViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ContaOnline.Domain.Models;
+
+namespace ContaOnline.Domain.ViewModels
+{
+    public class ContaViewModel
+    {
+        public ContaViewModel()
+        {
+            this.ContaCorrenteList = new List<ContaCorrente>();
+            this.ContaCategoriaList = new List<ContaCategoria>();
+            this.ContatoList = new List<Contato>();
+            this.ContaInstancia = new Conta();
+
+            this.ContaInstancia.DataVencimento = DateTime.Now;
+            this.ContaInstancia.Tipo = PagarReceber.Pagar;
+        }
+
+        public Conta ContaInstancia { get; set; }
+        public List<ContaCorrente> ContaCorrenteList { get; set; }
+        public List<ContaCategoria> ContaCategoriaList { get; set; }
+        public List<Contato> ContatoList { get; set; }
+
+    }
+}
--- ContaOnline.UI.Web/Controllers/ContaController.cs
+++ ContaOnline.UI.Web/Controllers/ContaController.cs
@@ -1,5 +1,7 @@
 ﻿using ContaOnline.Domain.Interfaces;
 using ContaOnline.Domain.Models;
+using ContaOnline.Domain.ViewModels;
+using ContaOnline.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContaOnline.UI.Web.Controllers
@@ -12,18 +14,90 @@ namespace ContaOnline.UI.Web.Controllers
         public ContaController()
         {
             _contaRepository = AppHelper.ObterContaRepository();
-            
+
         }
         public IActionResult Inicio()
         {
             _usuario = AppHelper.ObterUsuarioLogado(User);
             if (_usuario == null)
             {
-                return  RedirectToAction("Login","App");
+                return RedirectToAction("Login", "App");
             }
 
             var lista = _contaRepository.ObterPorUsuario(_usuario.Id);
             return View(lista);
+        }
+
+        public IActionResult Incluir()
+        {
+            _usuario = AppHelper.ObterUsuarioLogado(User);
+            if (_usuario == null) return RedirectToAction("Login", "App");
+            var viewModel = new ContaViewModel();
+
+            PreencherViewModel(viewModel);
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Incluir(ContaViewModel viewModel)
+        {
+            _usuario = AppHelper.ObterUsuarioLogado(User);
+            if (_usuario == null) return RedirectToAction("Login", "App");
+            try
+            {
+                viewModel.ContaInstancia.UsuarioId = _usuario.Id;
+                viewModel.ContaInstancia.Id =Guid.NewGuid().ToString();
+                _contaRepository.Incluir(viewModel.ContaInstancia);
+                return RedirectToAction("Inicio");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Ocorreu um erro ao incluir a conta. Tente novamente.");
+            }
+            PreencherViewModel(viewModel);
+            return View(viewModel);
+        }
+
+        public IActionResult Alterar(string id)
+        {
+            _usuario = AppHelper.ObterUsuarioLogado(User);
+            if (_usuario == null) return RedirectToAction("Login", "App");
+            var viewModel = new ContaViewModel();
+            viewModel.ContaInstancia = _contaRepository.ObterPorId(id);
+            PreencherViewModel(viewModel);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(ContaViewModel viewModel)
+        {
+            _usuario = AppHelper.ObterUsuarioLogado(User);
+            if (_usuario == null) return RedirectToAction("Login", "App");
+            try
+            {
+                viewModel.ContaInstancia.UsuarioId = _usuario.Id;                
+                _contaRepository.Alterar(viewModel.ContaInstancia);
+                return RedirectToAction("Inicio");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Ocorreu um erro ao incluir a conta. Tente novamente.");
+            }
+            PreencherViewModel(viewModel);
+            return View(viewModel);
+        }
+
+        private void PreencherViewModel(ContaViewModel viewModel)
+        {
+            var contaCorrenteRepository = AppHelper.ObterContaCorrenteRepository();
+            viewModel.ContaCorrenteList = contaCorrenteRepository.ObterTodos(_usuario.Id).ToList();
+
+            var contaCategoriaRepository = AppHelper.ObterContaCategoriaRepository();
+            viewModel.ContaCategoriaList = contaCategoriaRepository.ObterTodos(_usuario.Id).ToList();
+
+            var contatoRepository = AppHelper.ObterContatoRepository();
+            viewModel.ContatoList = contatoRepository.ObterTodos(_usuario.Id).ToList();
         }
     }
 }
