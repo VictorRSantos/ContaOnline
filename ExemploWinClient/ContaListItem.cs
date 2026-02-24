c22a1e47682f5cc3df8f628e816183b8ce69a1cf
Victor R Santos <vrsisa@hotmail.com>
2026-02-23 22:48:59 -0300

    criando um client para teste

---
+++ ExemploWinClient/ContaListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExemploWinClient
+{
+    public enum PagarReceber
+    {
+        Pagar = 1,
+        Receber = 2
+    }
+
+    public class ContaListItem
+    {        
+        public DateTime Data { get; set; }
+        public PagarReceber Tipo { get; set; }
+        public string Descricao { get; set; } = string.Empty;
+        public string Contato { get; set; } = string.Empty;
+        public string Categoria { get; set; } = string.Empty;
+        public decimal Valor { get; set; }        
+    }
+}
---
+++ ExemploWinClient/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExemploWinClient
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient s_httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:7189/") // ensure matches server
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Show();
+            ExibirDadosAsync();
+        }
+     
+        private async Task ExibirDadosAsync()
+        {
+            try
+            {
+                var response = await s_httpClient.GetAsync("api/ContaService");
+                response.EnsureSuccessStatusCode();
+                var contas = await response.Content.ReadAsAsync<List<ContaListItem>>();
+                dataGridView1.DataSource = contas;
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Unable to reach API: {ex.Message}");
+            }
+        }
+
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
